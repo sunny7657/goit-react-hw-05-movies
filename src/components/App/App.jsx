21db83f3f16487032dc6c5ea91b9610064ac0d7c
sunny7657 <ksvetlana466@gmail.com>
2024-01-28 14:06:50 +0100

    upd

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,9 +1,23 @@
+import { Cast } from 'components/Cast/Cast';
+import { Header } from 'components/Heaser/Header';
 import { Home } from 'components/Home/Home';
+import { Movie } from 'components/Movie/Movie';
+import { Movies } from 'components/Movies/Movies';
+import { Reviews } from 'components/Reviews/Reviews';
+import { Navigate, Route, Routes } from 'react-router-dom';
 
 export const App = () => {
   return (
-    <>
-      <Home />
-    </>
+    <Routes>
+      <Route path="/" element={<Header />}>
+        <Route index element={<Home />} />
+        <Route path="/movies" element={<Movies />} />
+        <Route path="/movies/:moviesId" element={<Movie />}>
+          <Route path="cast" element={<Cast />} />
+          <Route path="reviews" element={<Reviews />} />
+        </Route>
+      </Route>
+      <Route path="*" element={<Navigate to="/" />} />
+    </Routes>
   );
 };
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -26,12 +26,6 @@ export const Home = () => {
 
   return (
     <>
-      <header className="Header">
-        <nav className="HeaderNav">
-          <h2>Home</h2>
-          <h2>Movies</h2>
-        </nav>
-      </header>
       <Section>
         <Container>
           {loader && <AppLoader />}
