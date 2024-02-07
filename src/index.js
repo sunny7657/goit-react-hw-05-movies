91a679cdc6f675381448e52dff90109a0a69085f
sunny7657 <ksvetlana466@gmail.com>
2024-02-07 18:43:18 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,8 +5,9 @@ import './index.css';
 import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
+  <BrowserRouter basename="/goit-react-hw-05-movies">
     <App />
   </BrowserRouter>
 );
+
 // basename = '/goit-react-hw-05-movies';
